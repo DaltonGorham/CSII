bbb13b8c24f4b7b5634b00d7a18fcf4096a930d3
DaltonGorham <Daltongorham@gmail.com>
2024-07-09 11:33:49 -0500

    added menu

--- Module_1_Class/pointers-structs-arrays.cpp
+++ Module_1_Class/pointers-structs-arrays.cpp
@@ -7,34 +7,73 @@ int findSum(int arr[], int size);
 double findAverage(int arr[], int size);
 int findHighest(int arr[], int size);
 int findLowest(int arr[], int size);
+int getAmount();
 int main(){
 
-  practiceArrays();
-  
+  int menu;
 
+  do {
+    cout << "1: Practice Arrays " << endl;
+    cout << "2: Practice Pointers " << endl;
+    cout << "3: Practice Structs " << endl;
+    cout << "0: Quit" << endl;
 
+    cin >> menu;
 
+  switch (menu){
+    case 1: practiceArrays(); break;
+    case 2: practicePointers(); break;
+    case 3: practiceStructs(); break;
+    case 0: break;
+  };
+  } while (menu > 0);
 
   return 0;
 }
 
 
+
 void practiceArrays(){
-  int numbers[5];
-  cout << "Enter 5 integers" << endl;
-  for (int i = 0; i < 5; i++){
+  int size = getAmount();
+  int* numbers = new int[size];
+  cout << "Enter " << size << " integers" << endl;
+  for (int i = 0; i < size; i++){
     cin >> numbers[i];
   }
 
-  cout << "The sum is: " << findSum(numbers, 5) << endl;
-  cout << "The average is: " << findAverage(numbers, 5) << endl;
-  cout << "The Highest is: " << findHighest(numbers, 5) << endl;
-  cout << "The Lowest is: " << findLowest(numbers, 5) << endl;
+  cout << "The sum is: " << findSum(numbers, size) << endl;
+  cout << "The average is: " << findAverage(numbers, size) << endl;
+  cout << "The Highest is: " << findHighest(numbers, size) << endl;
+  cout << "The Lowest is: " << findLowest(numbers, size) << endl;
 
-
+  delete []numbers;
 }
 
 void practicePointers(){
+  string name;
+  int age;
+  cout << "What is your name? " << endl;
+  cin >> name;
+  cout << "What is your age?" << endl;
+  cin >> age;
+
+  cout << "Name: " << name << endl << "Address: " << &name << endl;
+
+  cout << "Age: " << age << endl << "Address: " << &age << endl;
+
+
+  string months[12] =
+   { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+  for (int i = 0; i < 12; i++){
+    cout << *(months + i) << " ";
+  }
+  cout << endl;
+  for (int i = 11; i > -1; i--){
+    cout << *(months + i) << " ";
+  }
+  cout << endl;
+
 
 }
 
@@ -47,7 +86,13 @@ void practiceStructs(){
 
 
 
+int getAmount(){
+  int amount;
+  cout << "How many numbers do you want? " << endl;
+  cin >> amount;
 
+  return amount;
+}
 
 int findSum(int arr[], int size){
   int sum;
